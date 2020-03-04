791258266aa9e706f308d3c99d0a7cdd093b64e8
bobosun0713 <chiabo.sun@gmail.com>
2020-03-04 15:19:39 +0800

    首頁-尋鬼探險

--- dev/js/hamburger.js
+++ dev/js/hamburger.js
@@ -1,17 +1,14 @@
+window.addEventListener('load', function() {
+    let hamburger = document.getElementById('hamburger')
+    let hamburgerMenu = document.getElementById('rwdHamburgerMenu')
 
-
-window.addEventListener('load',function(){
-
-  let hamburger = document.getElementById("hamburger");
-  let hamburgerMenu = document.getElementById("rwdHamburgerMenu");
-
-  hamburger.onclick = function(){
-    if( hamburger.classList.contains("is-active")){
-        hamburger.classList.remove("is-active");
-        hamburgerMenu.style.left = "100%"; 
-    }else{
-        hamburger.classList.add("is-active");
-        hamburgerMenu.style.left = "0%"; 
+    hamburger.onclick = function() {
+        if (hamburger.classList.contains('is-active')) {
+            hamburger.classList.remove('is-active')
+            hamburgerMenu.style.left = '100%'
+        } else {
+            hamburger.classList.add('is-active')
+            hamburgerMenu.style.left = '0%'
+        }
     }
-  }
-});
+})
