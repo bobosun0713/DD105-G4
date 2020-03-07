0e6a1ff660ccb6ae44806107ca96e3d99548f957
Trash0430Lily <fd90278@gmail.com>
2020-03-07 17:56:34 +0800

    index RWD完成

--- dev/js/indexSpotCarasoul.js
+++ dev/js/indexSpotCarasoul.js
@@ -2,18 +2,21 @@ function $id(id) {
     return document.getElementById(id)
 }
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
     let curIndex = 0
     let spotDisplay = document.querySelector('#spotInfoWrap')
     let spell1 = document.querySelector('#spell1')
     let spell2 = document.querySelector('#spell2')
     let spell3 = document.querySelector('#spell3')
 
+    
+
     spell1.style.opacity = 1
 
-    $id('leftScroll').onclick = function() {
+    //點擊輪播左右滑動，符咒消失出現
+    $id('leftScroll').onclick = function () {
         curIndex = curIndex + 1
-        spotDisplay.style.left = -470 * curIndex + 'px'
+        spotDisplay.style.left = -410 * curIndex + 'px'
         $id('rightScroll').disabled = false
         if (curIndex == 2) {
             $id('leftScroll').disabled = true
@@ -25,9 +28,9 @@ window.addEventListener('load', function() {
         }
     }
 
-    $id('rightScroll').onclick = function() {
+    $id('rightScroll').onclick = function () {
         curIndex = curIndex - 1
-        spotDisplay.style.left = -470 * curIndex + 'px'
+        spotDisplay.style.left = -410 * curIndex + 'px'
         $id('leftScroll').disabled = false
         if (curIndex == 0) {
             $id('rightScroll').disabled = true
@@ -39,33 +42,18 @@ window.addEventListener('load', function() {
         }
     }
 
-    // $id("loc1").onmouseover = function(e){
+    //mouse
+    let location = this.document.querySelectorAll('.location');
+    // console.log(location.length);
+    for (let i = 0; i < location.length; i++) {
 
-    //     e.target.nextElementSibling.style.opacity = 1;
-    //     console.log(e.target.nextElementSibling);
-    // }
+        location[i].onmouseover = function (e) {
+            e.target.nextElementSibling.style.opacity = 1;
+        }
 
-    // $id("loc1").onmouseout = function(e){
-    //     e.target.nextElementSibling.style.opacity = 0;
-    //     console.log(e.target.nextElementSibling);
-    // }
+        location[i].onmouseout = function (e) {
+            e.target.nextElementSibling.style.opacity = 0;
+        }
+    }
 
-    //點符咒右邊輪播滑動 現在寫不出來
-    // $id("spell1").onclick = function(){
-    //     spell1.style.opacity = 1;
-    //     spell2.style.opacity = 0;
-    //     spell3.style.opacity = 0;
-    // }
-
-    // $id("spell2").onclick = function(){
-    //     spell1.style.opacity = 0;
-    //     spell2.style.opacity = 1;
-    //     spell3.style.opacity = 0;
-    // }
-
-    // $id("spell3").onclick = function(){
-    //     spell1.style.opacity = 0;
-    //     spell2.style.opacity = 0;
-    //     spell3.style.opacity = 1;
-    // }
 })
