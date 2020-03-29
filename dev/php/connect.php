24eff399ebc1a979d1782874c51bc9cdd0bab288
Trash0430Lily <fd90278@gmail.com>
2020-03-29 16:34:46 +0800

    merge

--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 $dsn = "mysql:host=localhost; port=3306; dbname=dd105G4; charset=utf8";
@@ -9,12 +8,3 @@ $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE => 
 $pdo = new PDO($dsn, $user, $password, $options);
 
 ?>
-=======
-<?php 
-    $dsn="mysql:host=localhost;port=3306;dbname=dd105_G4;charset=utf8";
-    $user="root";
-    $password="root";
-    $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
-    $pdo=new pdo($dsn,$user,$password,$options);
- ?>
->>>>>>> cbb2df8539cb894e3d8c6ca7616e231137d79351
