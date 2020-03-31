2b0a693a120f0c34387af5fbb306fed6c6300aba
bobosun0713 <chiabo.sun@gmail.com>
2020-03-31 10:14:47 +0800

    首頁討論去按鈕

--- dev/php/back_forum.php
+++ dev/php/back_forum.php
@@ -2,7 +2,7 @@
 try{
     require_once("connect.php");
     // $sql = "select * from forum_report;";
-    $sql = "select M.forum_report_no , F.forum_no ,F.forum_title , F.images , MM.mem_name , M.forum_report_reason 
+    $sql = "select DISTINCT M.forum_report_no , F.forum_no ,F.forum_title , F.images , MM.mem_name , M.forum_report_reason , F.forum_status
     from forum_report M , forum F , member MM where M.forum_no = F.forum_no and F.mem_no = MM.mem_no";
     $forum = $pdo->query($sql);
     //自資料庫中取回資料
