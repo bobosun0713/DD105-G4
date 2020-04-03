4260108d5461bab99899ae2b41e6b46840d52f80
bobosun0713 <chiabo.sun@gmail.com>
2020-04-03 12:05:49 +0800

    討論區

--- dev/php/back_forum_delete.php
+++ dev/php/back_forum_delete.php
@@ -6,7 +6,7 @@ $memNo = $_SESSION["mem_no"];
 try{
     require_once("connect.php");
     // $sql = "delete from forum_report where forum_report_no = : and forum_report_name = :forum_report_name";
-    $sql = "delete from forum_report r , forum f where forum_report_no = :forum_report_no and mem_no = :mem_no ";
+    $sql = "delete from forum_report where forum_report_no = :forum_report_no and mem_no = :mem_no ";
     //== 修改文章狀態
     $forum = $pdo->prepare( $sql );
     $forum -> bindValue(":forum_report_no", $_POST["forum_report_no"]);
--- dev/php/connect.php
+++ dev/php/connect.php
@@ -1,11 +1,7 @@
 <?php 
-    $dsn="mysql:host=localhost;port=3306;dbname=dd105_G4;charset=utf8";
-<<<<<<< HEAD
+    $dsn="mysql:host=localhost;port=3306;dbname=dd105G4;charset=utf8";
     $user="root";
-=======
-    $user="lily1";
->>>>>>> 52e76ef90dcaf2ea41eab45ee1acee9700e5f015
-    $password="0000";
+    $password="root";
     $options=array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
     $pdo=new pdo($dsn,$user,$password,$options);
  ?>
