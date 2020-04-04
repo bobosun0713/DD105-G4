abfba93c7caf5e3e7859fb82593dc7d6d838ac65
ttaacc <sophieampa524@gmail.com>
2020-04-04 11:05:02 +0800

    ...會員影藏沒做完的地方

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var jshint = require("gulp-jshint")
 var sourcemaps = require("gulp-sourcemaps")
 var browserSync = require("browser-sync").create()
 var reload = browserSync.reload
-// var connectPHP = require('gulp-connect-php')
+var connectPHP = require('gulp-connect-php')
 
 //path 路徑
 var web = {
@@ -42,29 +42,29 @@ var options = {
 // }
 
 //流程
-gulp.task("concatjs", function() {
+gulp.task("concatjs", function () {
     gulp.src(web.js).pipe(gulp.dest("dest/js"))
 })
 
-gulp.task("img", function() {
+gulp.task("img", function () {
     gulp.src(web.img).pipe(gulp.dest("dest/img"))
 })
 
-gulp.task("font", function() {
+gulp.task("font", function () {
     gulp.src(web.font).pipe(gulp.dest("dest/font"))
 })
-gulp.task("music", function() {
+gulp.task("music", function () {
     gulp.src("dev/music/*.*").pipe(gulp.dest("dest/music"))
 })
-gulp.task("php", function() {
+gulp.task("php", function () {
     gulp.src("dev/*.php").pipe(gulp.dest("dest/"))
 })
-gulp.task("phpfolder", function() {
+gulp.task("phpfolder", function () {
     gulp.src("dev/php/*.*").pipe(gulp.dest("dest/php"))
 })
 
 //任務串連
-gulp.task("concatcss", ["sass"], function() {
+gulp.task("concatcss", ["sass"], function () {
     return gulp
         .src("css/*.css")
         .pipe(
@@ -75,14 +75,14 @@ gulp.task("concatcss", ["sass"], function() {
         .pipe(gulp.dest("dest/css"))
 })
 
-gulp.task("lint", function() {
+gulp.task("lint", function () {
     return gulp
         .src("./dev/js/*.js")
         .pipe(jshint())
         .pipe(jshint.reporter("default"))
 })
 
-gulp.task("sass", function() {
+gulp.task("sass", function () {
     return (
         gulp
             .src("dev/sass/*.scss")
@@ -96,7 +96,7 @@ gulp.task("sass", function() {
 
 //打包html
 
-gulp.task("fileinclude", function() {
+gulp.task("fileinclude", function () {
     gulp.src(["dev/*.html"])
         .pipe(
             fileinclude({
@@ -108,7 +108,7 @@ gulp.task("fileinclude", function() {
 })
 
 //壓縮圖片
-gulp.task("mini_img", function() {
+gulp.task("mini_img", function () {
     return gulp
         .src("dev/img/*.*")
         .pipe(imagemin())
@@ -121,17 +121,17 @@ gulp.task("mini_img", function() {
 //   gulp.watch(['*.html' , '**/*.html'],  ['fileinclude']);
 // });
 
-gulp.task("default", function() {
-    // browserSync.init({
-    //     server: {
-    //         baseDir: "./dest",
-    //         proxy: "localhost:8080",
-    //         port: 3000,
-    //         watch: true,
-    //         index: "index.html",
-    //     },
-    // })
-    // connectPHP.server(options)
+gulp.task("default", function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dest",
+            proxy: "localhost:8080",
+            port: 3000,
+            watch: true,
+            index: "index.html",
+        },
+    })
+    connectPHP.server(options)
     gulp.watch(web.html, ["fileinclude"]).on("change", reload)
     gulp.watch(web.sass, ["sass"]).on("change", reload)
     gulp.watch(web.js, ["concatjs"]).on("change", reload)
